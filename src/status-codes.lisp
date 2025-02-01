c7f4f1cf50334186490b919522a7be206fd9a983
Gabriel Johnson <gabriel.johnson.pitp@gmail.com>
2025-02-01 11:21:10 -0800

    Remove check-status-code and when-fail

--- src/status-codes.lisp
+++ src/status-codes.lisp
@@ -8,13 +8,6 @@
                    :int error-code
                    :string))
 
-(defun check-status-code (status-code if-error)
-  (unless (eql status-code +sqlite-ok+)
-    (funcall if-error (sqlite3-error-message status-code))))
-
-(defmacro when-fail ((message-variable form) &body body)
-  `(check-status-code ,form (lambda (,message-variable) ,@body)))
-
 (define-condition sqlite3-error (error)
   ((message :reader error-message
             :initarg :message)))
