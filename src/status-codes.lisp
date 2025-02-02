0f2ffb5c630ee8dfeed35caf94611d126ce5876a
Gabriel Johnson <gabriel.johnson.pitp@gmail.com>
2025-02-02 09:23:42 -0800

    Add more status codes

--- src/status-codes.lisp
+++ src/status-codes.lisp
@@ -3,6 +3,14 @@
 
 (defconstant +sqlite-ok+ 0)
 
+(defconstant +sqlite-busy+ 5)
+
+(defconstant +sqlite-row+ 100)
+
+(defconstant +sqlite-done+ 101)
+
+(defconstant +sqlite-misuse+ 21)
+
 (defun sqlite3-error-message (error-code)
   (foreign-funcall "sqlite3_errstr"
                    :int error-code
