ef4d8f098649e3821f489c540f5ad7f0697ea5ba
Gabriel Johnson <gabriel.johnson.pitp@gmail.com>
2025-02-04 14:11:07 -0800

    Refactor step-statement to use classes to indicate the result

--- src/statement.lisp
+++ src/statement.lisp
@@ -31,6 +31,8 @@
 (defmethod database-error-message ((statement unfinalized-statement))
   (database-error-message (statement-database statement)))
 
+(defgeneric step-statement (statement &key if-row if-done))
+
 ;; "Row pointer" actually points to a statement, since SQLite3 has you
 ;; access result rows through the statement. But this class is only
 ;; instantiated when sqlite3_step returns SQLITE_ROW.
@@ -63,33 +65,87 @@
 (defun do-nothing (&rest args)
   (declare (ignore args)))
 
+(defclass step-result ()
+  ((statement :reader result-statement
+              :initarg :statement)))
+
+(defmethod statement-pointer ((result step-result))
+  (statement-pointer (result-statement result)))
+
+(defmethod database-error-message ((result step-result))
+  (database-error-message (result-statement result)))
+
+(defgeneric act-on-step-result (step-result if-row if-done))
+
+(defclass statement-done (step-result)
+  ())
+
+(defmethod act-on-step-result ((result statement-done) if-row if-done)
+  (funcall if-done))
+
+(defclass statement-has-row (step-result)
+  ())
+
+(defmethod act-on-step-result ((result statement-has-row) if-row if-done)
+  (funcall if-row
+           (make-instance 'row :pointer (statement-pointer result))))
+
+(defclass statement-failed-busy (step-result)
+  ())
+
+;; The only time we're allowed to retry stepping a statement
+(defmethod act-on-step-result ((result statement-failed-busy) if-row if-done)
+  (restart-case (error 'step-statement-failed
+                       :statement (result-statement result)
+                       :message (database-error-message result))
+    (retry ()
+      :report "Try stepping the statement again."
+      (step-statement (result-statement result)
+                      :if-row if-row
+                      :if-done if-done))))
+
+(defclass statement-failed-misuse (step-result)
+  ())
+
+;; Can't treat this the same as other errors because, according to the
+;; docs, the error code and error message "may or may not be set"
+(defmethod act-on-step-result ((result statement-failed-misuse) if-row if-done)
+  ;; TODO: Replace this with whatever more helpful error class you
+  ;; come up with
+  (error 'sqlite-misuse-returned))
+
+(defclass statement-failed-error (step-result)
+  ())
+
+(defmethod act-on-step-result ((result statement-failed-error) if-row if-done)
+  (error 'step-statement-failed
+         :statement (result-statement result)
+         :message (database-error-message result)))
+
+(defmethod try-step-statement ((statement unfinalized-statement))
+  ;; It'd probably be better to defparameter an alist to determine the
+  ;; class, but it's annoying to construct those when the keys are
+  ;; variable/constant names rather than literals...
+  (make-instance (case (sqlite3-step (statement-pointer statement))
+                   (+sqlite-done+
+                    'statement-done)
+                   (+sqlite-row+
+                    'statement-has-row)
+                   (+sqlite-busy+
+                    'statement-failed-busy)
+                   (+sqlite-misuse+
+                    'statement-failed-misuse)
+                   (otherwise
+                    'statement-failed-error))
+                 :statement statement))
+
 (defmethod step-statement ((statement unfinalized-statement)
                            &key
                              (if-row #'do-nothing)
                              (if-done #'do-nothing))
-  (tagbody
-     retry
-     (case (sqlite3-step (statement-pointer statement))
-       (+sqlite-done+
-        (funcall if-done))
-       (+sqlite-row+
-        (funcall if-row
-                 (make-instance 'row
-                                :pointer (statement-pointer statement))))
-       (+sqlite-busy+
-        (restart-case (error 'step-statement-failed
-                             :statement statement
-                             :message (database-error-message statement))
-          (retry ()
-            :report "Try stepping the statement again"
-            (go retry))))
-       (+sqlite-misuse+
-        ;; TODO: Replace this with whatever more helpful error you come up with
-        (error 'sqlite-misuse-returned))
-       (otherwise
-        (error 'step-statement-failed
-               :statement statement
-               :message (database-error-message statement))))))
+  (act-on-step-result (try-step-statement statement)
+                      if-row
+                      if-done))
 
 (defmethod reset-statement ((statement unfinalized-statement))
   (foreign-funcall "sqlite3_reset"
