b60baad34b27df1a35ac01b24eb088ce4fd011ed
Gabriel Johnson <gabriel.johnson.pitp@gmail.com>
2025-05-01 15:45:03 -0700

    Remove unused function sqlite3-error-message

--- status-codes.lisp
+++ status-codes.lisp
@@ -11,11 +11,6 @@
 
 (defconstant +sqlite-misuse+ 21)
 
-(defun sqlite3-error-message (error-code)
-  (foreign-funcall "sqlite3_errstr"
-                   :int error-code
-                   :string))
-
 (define-condition sqlite3-error (error)
   ((message :reader error-message
             :initarg :message)))
